a30c3058c41e55358b0cbc74dd5c6cadfa5d15e9
Alberto Lerda <albertolerda97@gmail.com>
2022-01-17 14:18:53 +0100

    Timestamp agreement

--- app/roles/candidates.lua
+++ app/roles/candidates.lua
@@ -4,6 +4,50 @@ local rpc = require('cartridge.rpc')
 local log = require('log')
 
 local role_name = 'candidates'
+local function http_timestamp(req)
+  local result
+  -- number of confirmations
+  local limit = tonumber(req:stash('limit'))
+
+  local cs = rpc.get_candidates(role_name)
+
+  if limit > #cs - 1 then
+    result = "More confirmations than hosts"
+  elseif limit < 1 then
+    result = "At least a confirmation"
+  else
+    local confirm = 0 -- current number of confirmations
+    local i = 0
+    local ts, newts
+    local askedTo = {}
+
+    repeat -- until I have not enough confirmations
+      local c = cs[math.random(1,#cs)]
+      if not askedTo[c] then
+        -- found an host I haven't asked the timestamp yet
+        local opts = { prefer_local = false, leader_only = false,
+                       uri = c }
+        newts = rpc.call(role_name,
+	               'current_ts', {}, opts)
+        if i == 0 then
+          ts = newts
+        end
+        if ts == newts then
+          i = i+1
+        end
+        askedTo[c] = true
+      end
+    until ts ~= newts or i >= limit
+
+    if i == limit then
+      result = {ts, askedTo}
+    else
+      result = {"No agreement", askedTo}
+    end
+  end
+
+  return req:render{ json = result }
+end
 
 local function init()
     local httpd = cartridge.service_get('httpd')
@@ -16,18 +60,12 @@ local function init()
             return req:render{ json = rpc.get_candidates(role_name) }
         end
         )
+
+	-- ts = timestamp
         httpd:route({
-            method = 'GET', path = '/iam', public = true,
+            method = 'GET', path = '/ts/:limit', public = true,
         },
-        function(req)
-	    local cs = rpc.get_candidates(role_name)
-	    local opts = { prefer_local = false, leader_only = false,
-	                   uri = 'localhost:3302' }
-            return req:render{ 
-		    json = rpc.call(role_name,
-		                    'current_ts', {}, opts) 
-	    }
-        end
+        http_timestamp
         )
     end
 end
@@ -37,8 +75,9 @@ end
 
 
 local function current_ts()
-    log.info('REQUESTED TIMESTAMP');
-    return os.time()
+  local ts = os.time();
+  log.info('REQUESTED TIMESTAMP ' .. tostring(ts));
+  return ts
 end
 
 return {
